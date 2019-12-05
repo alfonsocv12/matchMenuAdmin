654c46d9ca6aea371e7f50392baf46c542bdb1f8
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-12-04 17:44:48 -0700

    Listo

--- app/src/main/java/com/example/matchamenueadmin/create.kt
+++ app/src/main/java/com/example/matchamenueadmin/create.kt
@@ -2,11 +2,22 @@ package com.example.matchamenueadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_create.*
 
 class create : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+        saveBtn()
+    }
+
+    private fun saveBtn(){
+        btnSave.setOnClickListener {
+            
+        }
     }
 }
--- app/src/main/java/com/example/matchamenueadmin/login.kt
+++ app/src/main/java/com/example/matchamenueadmin/login.kt
@@ -1,5 +1,6 @@
 package com.example.matchamenueadmin
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,10 @@ class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+    }
+
+    private fun saveUidShared(uid: String){
+        val prefs: SharedPreferences = contex
     }
 }
