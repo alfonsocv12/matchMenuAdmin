b15c8a77cd87e09449b33981a30e9cdb778875d2
Luis Angel Ortega <angel-ortega12@hotmail.com>
2019-12-04 19:38:16 -0700

    Alambre

--- app/src/main/java/com/example/matchamenueadmin/MainActivity.kt
+++ app/src/main/java/com/example/matchamenueadmin/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.example.matchamenueadmin
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        itemBtnAction()
+
+        val sharedPreferences = getSharedPreferences("MatchaMenuAdmin", Context.MODE_PRIVATE)
+        var uid = sharedPreferences.getString("uid","")
+
+        val elements = ArrayList<String>()
+
+        try {
+            db.collection("restaurant")
+                .whereEqualTo("id",uid)
+                .get()
+                .addOnSuccessListener { docs ->
+                    for (doc in docs!!) {
+                        elements.add(doc.getString("name").toString())
+                    }
+                    dishList.adapter = ArrayAdapter(this, R.layout.dish_list,elements)
+                }
+        } catch (err: Error) {
+            Toast.makeText(this, "¡Ups! No tienes platillos en tu menú", Toast.LENGTH_LONG).show()
+        }
+
     }
 
-    private fun itemBtnAction(){
-        btnNewItem.setOnClickListener {
-            startActivity(Intent(this, create::class.java))
-        }
-    }
 }
