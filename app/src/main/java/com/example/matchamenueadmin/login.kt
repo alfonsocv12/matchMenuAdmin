123906263bb499386e6089f65cd27450ae1572a7
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-12-04 17:48:31 -0700

    listo

--- app/src/main/java/com/example/matchamenueadmin/login.kt
+++ app/src/main/java/com/example/matchamenueadmin/login.kt
@@ -2,14 +2,21 @@ package com.example.matchamenueadmin
 
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import android.os.Bundle
 
 class login : AppCompatActivity() {
 
+    val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        
+
+    }
+
+    private  fun getUser(email: String){
+        db.collection('restaurant').whereEqualTo('email',email)
     }
 
     private fun saveUidShared(uid: String){
